1be718b9367ba97dec493f40da2f78e763416582
pedrofragata <pedro.fragata@hotmail.com>
2019-12-15 16:51:28 +0000

    authBase

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,58 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+class User {
+  constructor(
+    username,
+    password
+    ){
+    this.username=username,
+    this.password=password   
+  }
+}
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    users:[],
+    loggedUser: {}
+
+  },
+  mutations: {
+    //regist user
+    ADD_USER(state, user) {
+      let newUser= new User (user.username, user.password)
+      state.users.push(newUser)
+      console.log(state.users)
+    },
+    //login
+    LOGIN(state, payload){
+      for (let user in state.users){
+        if(user.username==payload.Lusername && user.password==payload.Lpassword)
+         {
+           state.loggedUser= state.users[user] // obtem-se o user loggado
+           console.log("LOGIn",state.loggedUser)
+         }
+      }
+
+    },
+    //logout
+    LOGOUT(state){
+      state.loggedUser={}
+      console.log("LOGOUT",state.loggedUser)
+
+    },
+
+  },
+  getters: {
+    //return loggedUser
+    isLogged: state =>{
+      return state.loggedUser
+    }
+  },
+
+
+
+
   actions: {},
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+      <!-- REGISTO-->
+     <label for="username">username</label>
+     <input id="username" type="text" v-model="newUsername">
+     <br>
+     <label for="password">password</label>
+     <input id="password" type="password" v-model="newPassword">
+     <br>
+     <input type="button" value="REGISTAR" @click="addUser">
+     <br>
+     <br>
+     <!-- LOGIN -->
+      <label for="Lusername">username</label>
+     <input id="Lusername" type="text" v-model="Lusername">
+     <br>
+     <label for="Lpassword">password</label>
+     <input id="Lpassword" type="password" v-model="Lpassword">
+     <br>
+     <input type="button" value="LOGIN" @click="login">
+     <br>
+     <br>
+    <!-- LOGOUT -->
+     <input v-if="isLoggedStatus" type="button" value="LOGOUT" @click="logout">
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
+
+class User {
+  constructor(
+    username,
+    password
+    ){
+    this.username=username,
+    this.password=password   
+  }}
 
 export default {
   name: "home",
   components: {
-    HelloWorld
-  }
+    
+  },
+  data(){
+    return {
+      newUsername:"",
+      newPassword:"",
+      Lusername:"",
+      Lpassword:"",
+      loggedUser: this.$store.getters.isLogged,
+      isLoggedStatus:false
+    }
+
+  },
+  methods: {
+    addUser(){
+      this.$store.commit("ADD_USER", new User(this.newUsername, this.newPassword))
+    },
+    login(){
+      this.$store.commit("LOGIN",(this.Lusername, this.Lpassword))
+      this.isLoggedStatus=true
+    },
+    logout(){
+      this.$store.commit("LOGOUT")
+      this.isLoggedStatus=true
+    }
+  },
 };
 </script>
