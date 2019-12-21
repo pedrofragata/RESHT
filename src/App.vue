5e83086ab3dbd8e5b1b4a8a762ef69a34d09654b
Nuno <9170511@esmad.ipp.pt>
2019-12-21 00:08:10 +0000

    ajustes na secção prato do dia, começar secção de reservas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="has-navbar-fixed-top">
+  <div id="app" class="has-navbar-fixed-top has-background-black-ter">
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,28 +6,128 @@
     </section>
     <div class="container is-fluid">
       <SectionHeader title="Prato do dia" />
-      <div class="columns ra-section-content">
+      <div class="columns">
         <div class="column is-4 is-offset-2">
-          <h3 class="title is-3 has-text-weight-bold ra-dish-name">Amêijoas à Bulhão Pato</h3>
-          <p class="is-size-5	ra-dish-desc">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices tincidunt arcu non sodales neque sodales. Pretium quam vulputate dignissim suspendisse in est ante in nibh. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum.
-          </p>
+          <h3
+            class="title is-3 has-text-weight-bold has-text-white ra-dish-name"
+          >Amêijoas à Bulhão Pato</h3>
+          <p
+            class="is-size-5 has-text-grey-light ra-dish-desc"
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices tincidunt arcu non sodales neque sodales. Pretium quam vulputate dignissim suspendisse in est ante in nibh. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum.</p>
           <div class="ra-day-selector-wrapper">
-            <button class="button">10</button>
-            <button class="button">11</button>
-            <button class="button">12</button>
+            <button class="button is-medium has-text-weight-semibold has-text-white is-primary">10</button>
+            <button class="button is-medium has-text-weight-semibold">11</button>
+            <button class="button is-medium has-text-weight-semibold">12</button>
           </div>
         </div>
         <div class="column is-4">
-          <img class="ra-disc-image" src="@/assets/ameijoas-a-bulhao-pato.jpg"/>
+          <img class="ra-dish-image" src="@/assets/ameijoas-a-bulhao-pato.jpg" />
+        </div>
+      </div>
+      <SectionHeader title="Reservar" />
+      <div class="columns">
+        <div class="column is-9 is-offset-1">
+          <form>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label has-text-grey-lighter" for="ra-form-day">Horário</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <input id="ra-form-day" class="input" type="date" />
+                  </div>
+                </div>
+                <div class="field">
+                  <div class="control">
+                    <input id="ra-form-hour" class="input" type="time" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label has-text-grey-lighter" for="ra-form-arrival">Hora de chegada</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <input id="ra-form-arrival" class="input" type="time" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label has-text-grey-lighter" for="ra-form-people">Nº de pessoas</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <input id="ra-form-people" class="input" type="number" value="1" min="1" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label has-text-grey-lighter" for="ra-form-dish">Prato 1</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <div class="select">
+                      <select id="ra-form-dish">
+                        <option value>Selecione o prato</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label has-text-grey-lighter" for="ra-form-health">Obs. de saúde</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <textarea id="ra-form-health" class="textarea" rows="1" placeholder="Dieta, alergénios, ..."></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label has-text-grey-lighter" for="ra-form-message">Outras observações</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <textarea id="ra-form-message" class="textarea" rows="2" placeholder="Espaço sossegado, junto à janela,  ..."></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label"></div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <input class="button has-background-grey-lighter has-text-grey-dark is-family-secondary is-size-5" type="submit" value="Enviar" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
   </div>
 
   <!-- <div class="home"> -->
-      <!-- REGISTO-->
-     <!-- <label for="username">username</label>
+  <!-- REGISTO-->
+  <!-- <label for="username">username</label>
      <input id="username" type="text" v-model="newUsername">
      <br>
      <label for="password">password</label>
@@ -35,9 +135,9 @@
      <br>
      <input type="button" value="REGISTAR" @click="addUser">
      <br>
-     <br> -->
-     <!-- LOGIN -->
-      <!-- <label for="Lusername">username</label>
+  <br>-->
+  <!-- LOGIN -->
+  <!-- <label for="Lusername">username</label>
      <input id="Lusername" type="text" v-model="Lusername">
      <br>
      <label for="Lpassword">password</label>
@@ -45,9 +145,9 @@
      <br>
      <input type="button" value="LOGIN" @click="login">
      <br>
-     <br> -->
-    <!-- LOGOUT -->
-     <!-- <input v-if="isLoggedStatus" type="button" value="LOGOUT" @click="logout"> -->
+  <br>-->
+  <!-- LOGOUT -->
+  <!-- <input v-if="isLoggedStatus" type="button" value="LOGOUT" @click="logout"> -->
 
   <!-- </div> -->
 </template>
@@ -64,7 +164,7 @@ import SectionHeader from "@/components/SectionHeader.vue";
 //     password
 //     ){
 //     this.username=username,
-//     this.password=password   
+//     this.password=password
 //   }}
 
 export default {
@@ -72,7 +172,7 @@ export default {
   components: {
     Nav,
     SectionHeader
-  },
+  }
   // data(){
   //   return {
   //     newUsername:"",
