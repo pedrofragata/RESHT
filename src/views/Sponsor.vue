f8d879639ac2f45c591594b3fd137e21160cf4a1
Nuno <9170511@esmad.ipp.pt>
2019-12-27 00:10:20 +0000

    página de patrocinadores

---
+++ src/views/Sponsor.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="sponsor">
+    <Nav />
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Produtos frescos e naturais</h1>
+          <h2 class="subtitle">Garantidos pelos nossos patrocinadores</h2>
+        </div>
+      </div>
+    </section>
+    <div class="container is-fluid">
+      <Divider title="Patrocinadores" />
+      <div class="columns">
+        <div class="column is-8 is-offset-2">
+          <div class="tile is-ancestor">
+            <SponsorTile
+              name="Pascoal"
+              image="http://pascoal.pt/Psc/wp-content/uploads/2018/04/Logo-Pascoal-miniatura512x512.png"
+            />
+            <SponsorTile
+              name="Pascoal"
+              image="http://pascoal.pt/Psc/wp-content/uploads/2018/04/Logo-Pascoal-miniatura512x512.png"
+            />
+            <SponsorTile
+              name="Pascoal"
+              image="http://pascoal.pt/Psc/wp-content/uploads/2018/04/Logo-Pascoal-miniatura512x512.png"
+            />
+            <SponsorTile
+              name="Pascoal"
+              image="http://pascoal.pt/Psc/wp-content/uploads/2018/04/Logo-Pascoal-miniatura512x512.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Nav from "@/components/Nav.vue";
+import Divider from "@/components/Divider.vue";
+import SponsorTile from "@/components/SponsorTile.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "sponsor",
+  components: {
+    Nav,
+    Divider,
+    SponsorTile,
+    Footer
+  }
+};
+</script>
+
+<style src="@/scss/sponsor.scss" lang="scss"></style>
