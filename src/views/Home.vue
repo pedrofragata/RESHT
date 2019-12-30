29d2ffda341db660ead99eba26a3a807e1a76eab
Pedro Fragata <36924906+pedrofragata@users.noreply.github.com>
2019-12-30 19:53:11 +0000

    limpeza de comentários

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -186,31 +186,6 @@
     <Footer />
   </div>
 
-  <!-- <div class="home"> -->
-  <!-- REGISTO-->
-  <!-- <label for="username">username</label>
-     <input id="username" type="text" v-model="newUsername">
-     <br>
-     <label for="password">password</label>
-     <input id="password" type="password" v-model="newPassword">
-     <br>
-     <input type="button" value="REGISTAR" @click="addUser">
-     <br>
-  <br>-->
-  <!-- LOGIN -->
-  <!-- <label for="Lusername">username</label>
-     <input id="Lusername" type="text" v-model="Lusername">
-     <br>
-     <label for="Lpassword">password</label>
-     <input id="Lpassword" type="password" v-model="Lpassword">
-     <br>
-     <input type="button" value="LOGIN" @click="login">
-     <br>
-  <br>-->
-  <!-- LOGOUT -->
-  <!-- <input v-if="isLoggedStatus" type="button" value="LOGOUT" @click="logout"> -->
-
-  <!-- </div> -->
 </template>
 
 <script>
@@ -222,14 +197,6 @@ import Carousel from "@/components/Carousel.vue";
 import Faq from "@/components/Faq.vue";
 import Footer from "@/components/Footer.vue";
 
-// class User {
-//   constructor(
-//     username,
-//     password
-//     ){
-//     this.username=username,
-//     this.password=password
-//   }}
 
 export default {
   name: "home",
@@ -241,30 +208,6 @@ export default {
     Faq,
     Footer
   }
-  // data(){
-  //   return {
-  //     newUsername:"",
-  //     newPassword:"",
-  //     Lusername:"",
-  //     Lpassword:"",
-  //     loggedUser: this.$store.getters.isLogged,
-  //     isLoggedStatus:false
-  //   }
-
-  // },
-  // methods: {
-  //   addUser(){
-  //     this.$store.commit("ADD_USER", new User(this.newUsername, this.newPassword))
-  //   },
-  //   login(){
-  //     this.$store.commit("LOGIN",(this.Lusername, this.Lpassword))
-  //     this.isLoggedStatus=true
-  //   },
-  //   logout(){
-  //     this.$store.commit("LOGOUT")
-  //     this.isLoggedStatus=true
-  //   }
-  // },
 };
 </script>
 
