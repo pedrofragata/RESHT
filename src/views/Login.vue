61ab6f199e4496407717af64ce2d31f87d153bfd
Pedro Fragata <36924906+pedrofragata@users.noreply.github.com>
2020-01-02 19:35:07 +0000

    swal e requireds

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,9 +10,10 @@
       <label for="tab-2" class="tab">Registar</label>
       <div class="login-form">
         <div class="sign-in-htm">
+          <form v-on:submit.prevent="login">
           <div class="group">
             <label for="logUsername" class="label">Nome de utilizador</label>
-            <input id="logUsername" type="text" class="input" v-model="logUsername" />
+            <input id="logUsername" type="text" class="input" v-model="logUsername" required />
           </div>
           <div class="group">
             <label for="logPassword" class="label">Password</label>
@@ -25,17 +26,23 @@
             />
           </div>
           <div class="group">
-            <input type="submit" class="button" value="Entrar" @click="login" />
+            <input type="submit" class="button" value="Entrar"/>
           </div>
           <div class="hr"></div>
           <div class="foot-lnk">
             <a href="#">Esqueceu-se da sua password?</a>
           </div>
+          </form>
         </div>
         <div class="sign-up-htm">
+          <form v-on:submit.prevent="addUser">
+          <div class="group">
+            <label for="email" class="label">Email</label>
+            <input id="email" type="email" class="input" v-model="email" required />
+          </div>
           <div class="group">
             <label for="newUsername" class="label">Nome de utilizador</label>
-            <input id="newUsername" type="text" class="input" v-model="newUsername" />
+            <input id="newUsername" type="text" class="input" v-model="newUsername" required />
           </div>
           <div class="group">
             <label for="newPassword" class="label">Password</label>
@@ -45,6 +52,7 @@
               class="input"
               data-type="password"
               v-model="newPassword"
+              required
             />
           </div>
           <div class="group">
@@ -55,35 +63,37 @@
               class="input"
               data-type="password"
               v-model="confPassword"
+              required
             />
           </div>
+
           <div class="group">
-            <label for="email" class="label">Email</label>
-            <input id="email" type="email" class="input" v-model="email" />
-          </div>
-          <div class="group">
-            <input type="submit" class="button" value="Registar" @click="addUser"/>
+            <input type="submit" class="button" value="Registar"/>
           </div>
           <div class="hr"></div>
+          </form>
         </div>
       </div>
     </div>
     <Footer />
   </div>
 </template>
+
+
 <script>
 //isto vai mudar
- class User {
-   constructor(
-     username,
-     password
-     ){
-     this.username=username,
-     this.password=password
-   }}
+class User {
+  constructor(username, password) {
+    (this.username = username), (this.password = password);
+  }
+}
 
 import Nav from "@/components/Nav.vue";
 import Footer from "@/components/Footer.vue";
+
+import Swal from "../../node_modules/sweetalert2/dist/sweetalert2.js";
+import "../../node_modules/sweetalert2/src/sweetalert2.scss";
+
 export default {
   components: {
     Nav,
@@ -94,9 +104,9 @@ export default {
       newUsername: "",
       newPassword: "",
       logUsername: "",
-	  logPassword: "",
-	  email:"",
-	  confPassword:"",
+      logPassword: "",
+      email: "",
+      confPassword: "",
       loggedUser: this.$store.getters.isLogged,
       isLoggedStatus: false
     };
@@ -107,6 +117,12 @@ export default {
         "ADD_USER",
         new User(this.newUsername, this.newPassword)
       );
+      Swal.fire({
+        icon: 'success',
+        title: 'Registo efetuado com sucesso!',
+        showConfirmButton: false,
+        timer: 1500
+      })
     },
     login() {
       this.$store.commit("LOGIN", (this.logUsername, this.logPassword));
