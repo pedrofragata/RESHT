ba270769834bca72c1820709c56ea1dd1f9530ad
Nuno <9170511@esmad.ipp.pt>
2020-01-03 18:53:06 +0000

    reduzir tamanho das imagens

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="owl-carousel owl-theme">
     <div class="item">
-      <img src="@/assets/abigail-keenan-WI_9GWX1o-0-unsplash.jpg" />
+      <img src="@/assets/abigail-keenan-WI_9GWX1o-0-unsplash2.jpg" />
     </div>
     <div class="item">
-      <img src="@/assets/cloris-ying-1Qt-W4BGrgc-unsplash.jpg" />
+      <img src="@/assets/cloris-ying-1Qt-W4BGrgc-unsplash2.jpg" />
     </div>
     <div class="item">
-      <img src="@/assets/michael-browning-MtqG1lWcUw0-unsplash.jpg" />
+      <img src="@/assets/michael-browning-MtqG1lWcUw0-unsplash2.jpg" />
     </div>
     <div class="item">
-      <img src="@/assets/nathan-dumlao-X_bbCAYzlTs-unsplash.jpg" />
+      <img src="@/assets/nathan-dumlao-X_bbCAYzlTs-unsplash2.jpg" />
     </div>
     <div class="item">
-      <img src="@/assets/nils-stahl-BCkLxilDvJU-unsplash.jpg" />
+      <img src="@/assets/nils-stahl-BCkLxilDvJU-unsplash2.jpg" />
     </div>
     <div class="item">
-      <img src="@/assets/zakaria-zayane-0uAVsDcyD0M-unsplash.jpg" />
+      <img src="@/assets/zakaria-zayane-0uAVsDcyD0M-unsplash2.jpg" />
     </div>
   </div>
 </template>
