64de2ae17261167ccc62116695dc9700024cb4cf
Nuno <9170511@esmad.ipp.pt>
2020-01-07 20:18:40 +0000

    ficheiro scss de about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sponsor">
+  <div class="about">
     <Nav />
     <div class="container is-fluid">
         <Divider title="Sobre nós" />
@@ -44,4 +44,4 @@ export default {
 }
 </script>
 
-<style></style>
+<style src="@/scss/about.scss" lang="scss"></style>
