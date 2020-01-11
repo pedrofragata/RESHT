001e48151849a55c0e234e802f5c80af7dd7e24f
Nuno <9170511@esmad.ipp.pt>
2020-01-11 20:57:58 +0000

    teste com interactjs

--- src/components/Draggable.vue
+++ src/components/Draggable.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="ra-draggable-artboard">
-    <div id="draggable1" class=".draggable">
-        Draggable
-    </div>
-    <div id="draggable2" class=".draggable">
-      Draggable
+    <div v-for="table in tables" :key="table.id">
+      <div :id="table.id" class="draggable has-background-grey-light has-text-grey-dark">
+        {{table.desc}}
+      </div>
     </div>
   </div>
 </template>
@@ -16,8 +15,36 @@ export default {
   name: "Draggable",
   data() {
     return {
-      screenX: 0,
-      screenY: 0
+      tables: [
+        {
+          id: 0,
+          desc: "Mesa 1",
+          categoria: 1,
+          screenX: 0,
+          screenY: 0
+        },
+        {
+          id: 1,
+          desc: "Mesa 2",
+          categoria: 2,
+          screenX: 0,
+          screenY: 0
+        },
+        {
+          id: 2,
+          desc: "Mesa 3",
+          categoria: 2,
+          screenX: 0,
+          screenY: 0
+        },
+        {
+          id: 3,
+          desc: "Mesa 4",
+          categoria: 3,
+          screenX: 0,
+          screenY: 0
+        }
+      ]
     };
   },
   methods: {
@@ -29,38 +56,37 @@ export default {
           endOnly: true,
           elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
         },
-        autoScroll: true,
+        autoScroll: false,
 
         onmove: this.dragMoveListener,
         onend: this.onDragEnd
       });
     },
     dragMoveListener(event) {
-      var target = event.target,
-        x =
-          (parseFloat(target.getAttribute("data-x")) || this.screenX) +
-          event.dx,
-        y =
-          (parseFloat(target.getAttribute("data-y")) || this.screenY) +
-          event.dy;
-
+      const target = event.target;
+      const tableIndex = target.id;
+      const x =
+              (parseFloat(target.getAttribute("data-x")) || (this.tables[tableIndex].screenX)) +
+              event.dx,
+            y =
+              (parseFloat(target.getAttribute("data-y")) || (this.tables[tableIndex].screenY)) +
+              event.dy;
       target.style.webkitTransform = target.style.transform =
-        "translate(" + x + "px, " + y + "px)";
+        `translate(${x}px, ${y}px)`;
 
       target.setAttribute("data-x", x);
       target.setAttribute("data-y", y);
     },
     onDragEnd(event) {
       const target = event.target;
-      this.screenX = target.getBoundingClientRect().left;
-      this.screenY = target.getBoundingClientRect().top;
+      const tableIndex = target.id;
+      this.tables[tableIndex].screenX = target.getBoundingClientRect().left;
+      this.tables[tableIndex].screenY = target.getBoundingClientRect().top;
     }
   },
   mounted() {
-    const draggable1 = document.querySelector("#draggable1");
-    const draggable2 = document.querySelector("#draggable2");
-    this.initInteract(draggable1);
-    this.initInteract(draggable2);
+    const draggables = document.querySelectorAll(".draggable");
+    draggables.forEach(draggable => this.initInteract(draggable));
   }
 };
 </script>
