7817e8dbeee3acd69db12b8cc66e90035400eeb6
Nuno <9170511@esmad.ipp.pt>
2020-01-12 22:55:07 +0000

    terminar efeito de arrasto

--- src/components/Draggable.vue
+++ src/components/Draggable.vue
@@ -50,13 +50,14 @@ export default {
   methods: {
     initInteract(selector) {
       interact(selector).draggable({
-        inertia: true,
+        inertia: true, // animação de aceleração e desaceleração da forma se a ação de arrasto for interrompida em movimento
         restrict: {
-          restriction: "parent",
-          endOnly: true,
-          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+          restriction: ".ra-draggable-artboard",  // contentor limite das formas
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 } // manter formas totalmente dentro do contentor
         },
-        autoScroll: false,
+        autoScroll: false,  // não permite scroll enquanto arrasta a forma para um dos extremos direito ou inferior da página 
+                            // (permitia navegar para fora da página)
+        // possivelmente desnecessário em conjunto com o restrict.elementRect
 
         onmove: this.dragMoveListener,
         onend: this.onDragEnd
@@ -64,22 +65,31 @@ export default {
     },
     dragMoveListener(event) {
       const target = event.target;
-      const tableIndex = target.id;
+      const tableIndex = target.id; // o índice do objeto no array é igual ao id do seu elemento no html (pelo ciclo v-for)
+
+      // obter valores de x e y iniciais pelos atributos data-x e data-y (em string)
+      // os atributos não existem até serem criados após o primeiro movimento
+      // nesse caso, como fallback, associar o x e o y aos do objeto
+      // somar o delta entre a posição inicial e a posição atual, à posição inicial do rato
       const x =
               (parseFloat(target.getAttribute("data-x")) || (this.tables[tableIndex].screenX)) +
               event.dx,
             y =
               (parseFloat(target.getAttribute("data-y")) || (this.tables[tableIndex].screenY)) +
               event.dy;
+
+      // colocar a forma na nova posição por CSS
       target.style.webkitTransform = target.style.transform =
         `translate(${x}px, ${y}px)`;
 
+      // atualizar atributos do x e do y para a nova posição
       target.setAttribute("data-x", x);
       target.setAttribute("data-y", y);
     },
     onDragEnd(event) {
       const target = event.target;
       const tableIndex = target.id;
+      // atualizar os x e y no objeto
       this.tables[tableIndex].screenX = target.getBoundingClientRect().left;
       this.tables[tableIndex].screenY = target.getBoundingClientRect().top;
     }
