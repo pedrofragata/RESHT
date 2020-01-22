765be6f9696dc4effcb58b8c1804a8284655dd20
Pedro Fragata <36924906+pedrofragata@users.noreply.github.com>
2020-01-22 19:32:14 +0000

    EditProfile(halfway)

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,103 +1,190 @@
 <template>
-    <div>
-        <TheNav />
-        <div class="profile">
-            <div class="container is-fluid">
-                <div class="columns">
-                    <div class="column is-6 is-offset-1">
-                        <div class="box">
-                            <h4 class="title is-4 has-text-grey-light">Dados Pessoais</h4>
-                            <form>
-                                <div class="field is-horizontal">
-                                    <!-- div dummy -->
-                                    <div class="field-label">
-                                        <label class="label"></label>
-                                    </div>
-                                    <div class="field-body">
-                                        <div class="field">
-                                            <p class="control has-icons-left">
-                                                <img :src="photo" />
-                                                <span class="icon is-small is-pulled-right">
-                                                    <i class="fas fa-pencil-alt"></i>
-                                                </span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="field is-horizontal">
-                                    <div class="field-label is-normal">
-                                        <label class="label has-text-grey-lighter">Nome</label>
-                                    </div>
-                                    <div class="field-body">
-                                        <div class="field">
-                                            <p class="control has-icons-right">
-                                                <input class="input is-static" type="text" v-model="fullName" readonly>
-                                                <span class="icon is-small is-right">
-                                                    <i class="fas fa-pencil-alt"></i>
-                                                </span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="field is-horizontal">
-                                    <div class="field-label is-normal">
-                                        <label class="label has-text-grey-lighter">Sobre</label>
-                                    </div>
-                                    <div class="field-body">
-                                        <div class="field">
-                                            <div class="control">
-                                                <textarea class="textarea" v-model="about" readonly></textarea>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="field is-horizontal">
-                                    <div class="field-label is-normal">
-                                        <label class="label has-text-grey-lighter">Email</label>
-                                    </div>
-                                    <div class="field-body">
-                                        <div class="field">
-                                            <p class="control has-icons-left">
-                                                <input class="input is-static" type="email" v-model="email" readonly>
-                                                <span class="icon is-small is-left">
-                                                    <i class="fas fa-envelope"></i>
-                                                </span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="field is-horizontal">
-                                    <div class="field-label is-normal">
-                                        <label class="label has-text-grey-lighter">Palavra passe</label>
-                                    </div>
-                                    <div class="field-body">
-                                        <div class="field">
-                                            <p class="control has-icons-left has-icons-right">
-                                                <input class="input is-static" type="password" v-model="password" readonly>
-                                                <span class="icon is-small is-left">
-                                                    <i class="fas fa-key"></i>
-                                                </span>
-                                                <span class="icon is-small is-right">
-                                                    <i class="fas fa-pencil-alt"></i>
-                                                </span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+  <div>
+    <TheNav />
+    <div class="profile">
+      <div class="container is-fluid">
+        <div class="columns">
+          <div class="column is-6 is-offset-1">
+            <div class="box">
+              <h4 class="title is-4 has-text-grey-light">Dados Pessoais</h4>
+              <form>
+                <div class="field is-horizontal">
+                  <!-- div dummy -->
+                  <div class="field-label">
+                    <label class="label"></label>
+                  </div>
+                  <div class="field-body">
+                    <div class="field">
+                      <p class="control has-icons-left">
+                        <img :src="photo" />
+                        <span class="icon is-small is-pulled-right">
+                          <i class="fas fa-pencil-alt"></i>
+                        </span>
+                      </p>
                     </div>
-                    <div class="column is-4">
-                        <div class="box">
-                            <h4 class="title is-4 has-text-grey-light">Reservas</h4>
-                            <span>Ainda não efetuou reservas.</span>
-                        </div>
-                    </div>
+                  </div>
                 </div>
+                <!-- ------------------------ NAME ---------------------------- -->
+                <div class="field is-horizontal">
+                  <div class="field-label is-normal">
+                    <label class="label has-text-grey-lighter">Nome</label>
+                  </div>
+                  <div class="field-body">
+                    <div class="field">
+                      <p class="control has-icons-right">
+                        <input
+                          id="inputFullName"
+                          class="input"
+                          :class="{'is-static': editN}"
+                          type="text"
+                          v-model="fullName"
+                          :readonly="editN"
+                        />
+                      </p>
+                    </div>
+                    <!-- open modal -->
+                    <div class="field" v-if="editN==false">
+                      <button
+                        type="button"
+                        class="editButton is-family-secondary is-size-6"
+                        @click="activeModal=true"
+                      >Confirmar</button>
+                      <button
+                        type="button"
+                        class="cancelButton has-text-grey-dark is-family-secondary is-size-6"
+                        @click="editN = true"
+                      >Cancelar</button>
+                    </div>
+                    <!-- lapis -->
+                    <div class="field">
+                      <span class="icon is-small is-right" @click="editN = false">
+                        <i class="fas fa-pencil-alt"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <!-- EDIT NAME MODAL -->
+                  <div class="modal" :class="{'is-active': activeModal}" id="editNameModal">
+                    <div class="modal-background"></div>
+                    <div class="modal-card">
+                      <header class="modal-card-head">
+                        <p class="modal-card-title">Editar Nome</p>
+                        <button class="delete" aria-label="close"></button>
+                      </header>
+                      <section class="modal-card-body">
+                        <h2>Nome anterior: {{this.fullNameBefore}}</h2>
+                        <h2>Nome editado: {{this.fullName}}</h2>
+                        <!-- Content ... -->
+                      </section>
+                      <footer class="modal-card-foot">
+                        <button class="button is-success" @click="editFullName">Gravar alterações</button>
+                        <button type="button" class="button" @click="activeModal=false">Cancelar</button>
+                      </footer>
+                    </div>
+                  </div>
+                  <!-- END OF EDIT NAME  MODAL -->
+                </div>
+                <!-- ------------------- ABOUT ---------------------- -->
+                <div class="field is-horizontal">
+                  <div class="field-label is-normal">
+                    <label class="label has-text-grey-lighter">Sobre</label>
+                  </div>
+                  <div class="field-body">
+                    <div class="field">
+                      <div class="control">
+                        <textarea class="textarea" :class="{'is-static': editA}"  v-model="about" :readonly="editA" @click="editA=false" ></textarea>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="field is-horizontal">
+                  <div class="field-label is-normal">
+                    <label class="label has-text-grey-lighter">Email</label>
+                  </div>
+                  <div class="field-body">
+                    <div class="field">
+                      <p class="control has-icons-left">
+                        <input class="input is-static" type="email" v-model="email" readonly />
+                        <span class="icon is-small is-left">
+                          <i class="fas fa-envelope"></i>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <!-- ------------------------ PASSWORD --------------------------- -->
+                <div class="field is-horizontal">
+                  <div class="field-label is-normal">
+                    <label class="label has-text-grey-lighter">Palavra passe</label>
+                  </div>
+                  <div class="field-body">
+                    <div class="field">
+                      <p class="control has-icons-left has-icons-right">
+                        <input
+                          class="input"
+                          :class="{'is-static': editPw}"
+                          type="password"
+                          v-model="password"
+                          :readonly="editPw"
+                        />
+                        <span class="icon is-small is-left">
+                          <i class="fas fa-key"></i>
+                        </span>
+                      </p>
+                    </div>
+                    <div class="field" v-if="editPw==false">
+                      <button
+                        class="editButton is-family-secondary is-size-6"
+                        type="button"
+                        @click="activePwModal=true"
+                      >Confirmar</button>
+                      <button
+                        type="button"
+                        class="cancelButton has-text-grey-dark is-family-secondary is-size-6"
+                        @click="editPw = true"
+                      >Cancelar</button>
+                    </div>
+                    <!-- LAPIS -->
+                    <div class="field">
+                      <span class="icon is-small is-right" v-on:click="editPw = false">
+                        <i class="fas fa-pencil-alt"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <!-- EDIT PASSWORD MODAL -->
+                  <div class="modal" :class="{'is-active': activePwModal}" id="editPassWordModal">
+                    <div class="modal-background"></div>
+                    <div class="modal-card">
+                      <header class="modal-card-head">
+                        <p class="modal-card-title">Editar Password</p>
+                        <button class="delete" aria-label="close"></button>
+                      </header>
+                      <section class="modal-card-body">
+                        <h2>Password anterior: {{this.passwordBefore}}</h2>
+                        <h2>Nova password: {{this.password}}</h2>
+                        <!-- Content ... -->
+                      </section>
+                      <footer class="modal-card-foot">
+                        <button class="button is-success" @click="editPassword">Gravar alterações</button>
+                        <button type="button" class="button" @click="activePwModal=false">Cancelar</button>
+                      </footer>
+                    </div>
+                  </div>
+                  <!-- END OF EDIT PASSWORD  MODAL -->
+                </div>
+              </form>
             </div>
+          </div>
+          <div class="column is-4">
+            <div class="box">
+              <h4 class="title is-4 has-text-grey-light">Reservas</h4>
+              <span>Ainda não efetuou reservas.</span>
+            </div>
+          </div>
         </div>
-        <TheFooter />
+      </div>
     </div>
+    <TheFooter />
+  </div>
 </template>
 
 <script>
@@ -105,38 +192,73 @@ import TheNav from "@/components/layout/TheNav.vue";
 import TheFooter from "@/components/layout/TheFooter.vue";
 
 export default {
-    name: "Profile",
-    components: {
-        TheNav,
-        TheFooter
-    },
-    data(){
-        return{
-            users: [],
-            fullName:"",
-            email:"",
-            photo:"",
-            password:"",
-            about:"",
-            
-        }
-    },
-    created() {
-         this.users = this.$store.getters["users/allUsers"],
-         this.fullName = this.getUser(this.$route.params.userID).fullName
-         this.photo = this.getUser(this.$route.params.userID).avatar
-         this.email = this.getUser(this.$route.params.userID).email
-         this.password = this.getUser(this.$route.params.userID).password
-         this.about = this.getUser(this.$route.params.userID).about
+  name: "Profile",
+  components: {
+    TheNav,
+    TheFooter
+  },
+  data() {
+    return {
+      users: [],
+      fullName: "",
+      email: "",
+      photo: "",
+      password: "",
+      about: "",
+      editN: true,
+      editPw: true,
+      editA: true,
+      activeModal: false,
+      activePwModal: false,
+      fullNameBefore: "",
+      passwordBefore: ""
+    };
+  },
+  created() {
+    (this.users = this.$store.getters["users/allUsers"]),
+      (this.fullName = this.getUser(this.$route.params.userID).fullName);
+    this.photo = this.getUser(this.$route.params.userID).avatar;
+    this.email = this.getUser(this.$route.params.userID).email;
+    this.password = this.getUser(this.$route.params.userID).password;
+    this.about = this.getUser(this.$route.params.userID).about;
 
+    this.fullNameBefore = this.getUser(this.$route.params.userID).fullName;
+    this.passwordBefore = this.getUser(this.$route.params.userID).password;
+  },
+  methods: {
+    /*openEditNameModal(){
+        let modal = document.getElementById("editNameModal")
+        let html = document.querySelector('html');
+        modal.classList.add("is-active")
+        html.classList.add("is-clipped")
+    },*/
+    /*editTransform() {
+      let element = document.getElementById("inputFullName");
+      element.classList.remove("is-static");
+      this.edit = false;
+    },*/
+    editFullName() {
+      console.log(this.fullName)
+      this.$store.commit("users/EDIT_FULLNAME", {
+        uID: this.$route.params.userID,
+        fullName: this.fullName
+      });
+      this.$store.commit("users/SAVE_TO_LOCALSTORAGE");
     },
-    methods: {
-        getUser(id) {
-            return this.users.filter(user => user.uID == id)[0]
-        }
+
+    editPassword() {
+      this.$store.commit("users/EDIT_PASSWORD", {
+        uID: this.$route.params.userID,
+        password: this.password
+      });
+      this.$store.commit("users/SAVE_TO_LOCALSTORAGE");
+    },
+
+    getUser(id) {
+      return this.users.filter(user => user.uID == id)[0];
     }
-
-}
+  }
+};
 </script>
 
 <style src="@/scss/profile.scss" lang="scss" scoped></style>
