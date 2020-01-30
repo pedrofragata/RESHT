8d632b4674d51a2b1fcc87be324d06e2bda6b07d
Pedro Fragata <36924906+pedrofragata@users.noreply.github.com>
2020-01-30 23:44:54 +0000

    Commit Final

--- src/components/backoffice/RestaurantLayout.vue
+++ src/components/backoffice/RestaurantLayout.vue
@@ -355,9 +355,9 @@ export default {
     console.log(this.currentBooking);
 
     for (let i = 0; i < this.concurrentBookings.length; i++) {
-      console.log(this.concurrentBookings[i].tables.length, "CAOOOOOOO");
+      
       for (let j = 0; j < this.concurrentBookings[i].tables.length; j++) {
-        console.log(j, "JOTAAAAAAA");
+        
         this.allTables.filter(table => {
           if (table.tID == this.concurrentBookings[i].tables[j].tID) {
             return true;
