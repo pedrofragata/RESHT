8bc785481cf0c0053b88395f5e2287a31eecfa05
Pedro Fragata <36924906+pedrofragata@users.noreply.github.com>
2020-01-30 14:45:37 +0000

    minorChanges2

--- src/views/Backoffice.vue
+++ src/views/Backoffice.vue
@@ -2,89 +2,88 @@
   <div class="backoffice">
     <TheNav />
     <div class="container is-fluid is-relative">
-        <div class="columns">
-            <div class="column is-12 is-3-desktop">
-                <div class="box">
-                    <aside class="menu">
-                        <p class="menu-label">
-                            Geral
-                        </p>
-                        <ul class="menu-list">
-                            <li>
-                                <a class="is-active">Reservas</a>
-                                <ul>
-                                    <li><a>Lista</a></li>
-                                    <li><a>Atribuir mesa</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a class="is-active">Aplicação</a>
-                                <ul>
-                                    <li><a>Pratos e Menus</a></li>
-                                    <li><a>Galeria</a></li>
-                                    <li><a>FAQs</a></li>
-                                    <li><a>Patrocinadores</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <p class="menu-label">
-                            Administração
-                        </p>
-                        <ul class="menu-list">
-                            <li><a>Utilizadores</a></li>
-                        </ul>
-                    </aside>
-                </div>
-            </div>
-            <div class="column is-12 is-9-desktop">
-                <div class="box">
-                    <h3 class="title is-3 is-spaced has-text-centered">Reservas</h3>
-                    <FilterBookings @bookings-filter-applied="filterBookings" />
-                    <TableBookings :bookings="bookings" />
-                    <h5 v-if="activeBooking != null" class="subtitle is-5 has-text-centered">Atribuir mesa</h5>
-                    <RestaurantLayout v-if="activeBooking != null" class="is-hidden-touch" />
-                </div>
-                <div class="box">
-                    <h3 class="title is-3 is-spaced has-text-centered">Pratos e Menus</h3>
-                    <FilterDishes @dishes-filter-applied="filterDishes" />
-                    <TableDishes :dishes="dishes" />
-                    <div class="columns">
-                        <div class="column is-10 is-offset-1">
-                            <h5 class="subtitle is-5 has-text-centered">Criar ou alterar prato</h5>
-                            <FormDish />
-                            <h5 class="subtitle is-5 has-text-centered">Criar ou alterar menu</h5>
-                            <FormMenu @entry-changed="updateMenu" />
-                            <div class="columns ra-menu-wrapper">
-                                <div class="column is-10 is-offset-2">
-                                    <Menu :menu="menu" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="box">
-                    <h3 class="title is-3 is-spaced has-text-centered">Galeria</h3>
-                    <CarouselImages />
-                </div>
-                <div class="box">
-                    <h3 class="title is-3 is-spaced has-text-centered">FAQs</h3>
-                    <div class="columns">
-                        <div class="column is-10 is-offset-1">
-                            <FormFaqs />
-                        </div>
-                    </div>
-                </div>
-                <div class="box">
-                    <h3 class="title is-3 is-spaced has-text-centered">Utilizadores</h3>
-                     <FilterUsers @users-filter-applied="filterUsers" />
-                    <TableUsers :users="users" />
-                </div>
-            </div>
+      <div class="columns">
+        <div class="column is-12 is-3-desktop sticky">
+          <div class="box">
+            <aside class="menu">
+              <p class="menu-label">Geral</p>
+              <ul class="menu-list">
+                <li>
+                  <a class="is-active">Reservas</a>
+                  <ul>
+                    <router-link to="/backoffice/#bookingsSection">Lista</router-link>
+                    <li>
+                      <a>Atribuir mesa</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a class="is-active">Aplicação</a>
+                  <ul>
+                    <router-link to="/backoffice/#dishesSection">Pratos e Menus</router-link>
+                    <router-link to="/backoffice/#galeriaSection">Galeria</router-link>
+                    <router-link to="/backoffice/#faqsSection">Faqs</router-link>
+                    <li>
+                      <a>Patrocinadores</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <p class="menu-label">Administração</p>
+              <ul class="menu-list">
+                <router-link to="/backoffice/#usersSection">Utilizadores</router-link>
+              </ul>
+            </aside>
+          </div>
         </div>
+        <div class="column is-12 is-9-desktop">
+          <div class="box">
+            <h3 id="bookingsSection" class="title is-3 is-spaced has-text-centered">Reservas</h3>
+            <FilterBookings @bookings-filter-applied="filterBookings" />
+            <TableBookings :bookings="bookings" />
+            <h5 v-if="activeBooking != null" class="subtitle is-5 has-text-centered">Atribuir mesa</h5>
+            <RestaurantLayout v-if="activeBooking != null" class="is-hidden-touch" />
+          </div>
+          <div class="box">
+            <h3 id="dishesSection" class="title is-3 is-spaced has-text-centered">Pratos e Menus</h3>
+            <FilterDishes @dishes-filter-applied="filterDishes" />
+            <TableDishes :dishes="dishes" />
+            <div class="columns">
+              <div class="column is-10 is-offset-1">
+                <h5 class="subtitle is-5 has-text-centered">Criar ou alterar prato</h5>
+                <FormDish />
+                <h5 class="subtitle is-5 has-text-centered">Criar ou alterar menu</h5>
+                <FormMenu @entry-changed="updateMenu" />
+                <div class="columns ra-menu-wrapper">
+                  <div class="column is-10 is-offset-2">
+                    <Menu :menu="menu" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="galeriaSection" class="box">
+            <h3 class="title is-3 is-spaced has-text-centered">Galeria</h3>
+            <CarouselImages />
+          </div>
+          <div class="box">
+            <h3 id="faqsSection" class="title is-3 is-spaced has-text-centered">FAQs</h3>
+            <div class="columns">
+              <div class="column is-10 is-offset-1">
+                <FormFaqs />
+              </div>
+            </div>
+          </div>
+          <div class="box">
+            <h3 id="usersSection" class="title is-3 is-spaced has-text-centered">Utilizadores</h3>
+            <FilterUsers @users-filter-applied="filterUsers" />
+            <TableUsers :users="users" />
+          </div>
+        </div>
+      </div>
     </div>
     <TheFooter />
   </div>
-
 </template>
 
 <script>
@@ -105,51 +104,51 @@ import TableUsers from "@/components/backoffice/TableUsers.vue";
 import { mapGetters } from "vuex";
 
 export default {
-    name: "Backoffice",
-    components: {
-        TheNav,
-        FilterBookings,
-        FilterUsers,
-        TableBookings,
-        RestaurantLayout,
-        FilterDishes,
-        TableDishes,
-        FormDish,
-        FormMenu,
-        Menu,
-        CarouselImages,
-        FormFaqs,
-        TheFooter,
-        TableUsers
+  name: "Backoffice",
+  components: {
+    TheNav,
+    FilterBookings,
+    FilterUsers,
+    TableBookings,
+    RestaurantLayout,
+    FilterDishes,
+    TableDishes,
+    FormDish,
+    FormMenu,
+    Menu,
+    CarouselImages,
+    FormFaqs,
+    TheFooter,
+    TableUsers
+  },
+  data() {
+    return {
+      bookings: [],
+      dishes: [],
+      menu: [],
+      users: []
+    };
+  },
+  created() {
+    this.users = this.$store.getters["users/allUsers"];
+  },
+  methods: {
+    filterBookings(bookings) {
+      this.bookings = bookings;
     },
-    data() {
-        return {
-            bookings: [],
-            dishes: [],
-            menu: [],
-            users: []
-        }
+    filterDishes(dishes) {
+      this.dishes = dishes;
     },
-    created(){
-        (this.users = this.$store.getters["users/allUsers"])
+    updateMenu(menu) {
+      this.menu = menu;
     },
-    methods: {
-        filterBookings(bookings) {
-            this.bookings = bookings;
-        },
-        filterDishes(dishes) {
-            this.dishes = dishes;
-        },
-        updateMenu(menu) {
-            this.menu = menu;
-        },
-        filterUsers(users) {
-            this.users = users
-        }
-    },
-    computed:{
-        ...mapGetters("bookings", ["activeBooking"])
+    filterUsers(users) {
+      this.users = users;
     }
+  },
+  computed: {
+    ...mapGetters("bookings", ["activeBooking"])
+  }
 };
 </script>
 
