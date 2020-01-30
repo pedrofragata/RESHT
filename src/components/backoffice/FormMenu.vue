02de677b85a1e4ec225ba6eec63f0429c8958a93
Nuno <9170511@esmad.ipp.pt>
2020-01-30 14:42:50 +0000

    criar menu funcional (atualiza as datas do prato)

--- src/components/backoffice/FormMenu.vue
+++ src/components/backoffice/FormMenu.vue
@@ -1,13 +1,13 @@
 <template>
-    <form @submit.prevent="">
+    <form @submit.prevent="addMenu">
         <VInput id="ra-dish-menu-name" type="text" label="Nome:" @input-changed="updateDishName" />
         <VSplit id="ra-dish-availSince" label="Disponível desde:">
-            <input slot="first-field" id="ra-dish-availSince" class="input" type="date">
-            <input slot="second-field" class="input" type="time">
+            <input slot="first-field" id="ra-dish-availSince" class="input" type="date" v-model="dateAvailSince" required>
+            <input slot="second-field" class="input" type="time" v-model="timeAvailSince" required>
         </VSplit>
         <VSplit id="ra-dish-availUpTo" label="Disponível até:">
-            <input slot="first-field" id="ra-dish-availUpTo" class="input" type="date">
-            <input slot="second-field" class="input" type="time">
+            <input slot="first-field" id="ra-dish-availUpTo" class="input" type="date" v-model="dateAvailUpTo" required>
+            <input slot="second-field" class="input" type="time" v-model="timeAvailUpTo" required>
         </VSplit>
         <VSubmit value="Submeter" size="is-size-6 is-fullwidth" />
     </form>
@@ -30,7 +30,11 @@ export default {
     data() {
         return {
             dishName: "",
-            menu: []
+            menu: [],
+            dateAvailSince: "",
+            timeAvailSince: "",
+            dateAvailUpTo: "",
+            timeAvailUpTo: ""
         }
     },
     computed: {
@@ -40,6 +44,14 @@ export default {
         updateDishName(name) {
             const foundDish = this.dishByName(name);
             if (foundDish) this.menu.push(foundDish);
+        },
+        addMenu() {
+            this.$store.commit("dishes/ADD_MENU", {
+                menu: this.menu,
+                dateAvailableSince: `${this.dateAvailSince} ${this.timeAvailSince}`,
+                dateAvailableUpTo: `${this.dateAvailUpTo} ${this.timeAvailUpTo}`
+            });
+            this.$store.commit("dishes/SAVE_TO_LOCALSTORAGE");
         }
     },
     watch: {
